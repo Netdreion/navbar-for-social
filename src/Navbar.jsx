a07283322f9d034bb118b36a78ceeee00859da37
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-27 14:49:59 -0400

    adding links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
 
 import "./App.css";
 
 function Navbar() {
   return (
-    <>
-      <Navbar />
-    </>
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={reactLogo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container"></div>
+        <ul className="links">
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+          <li>
+            <a href="#">contact</a>
+          </li>
+          <li>
+            <a href="#">product</a>
+          </li>
+        </ul>
+      </div>
+      <ul className="social-icons">
+        <li>
+          <a href="https://www.twitter.com">
+            <FaTwitter />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
-export default App;
+export default Navbar;
