e263d437c0fc77de43f46c4183abd5f83eb37e48
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-30 22:23:53 -0400

    toogle Btn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
-import SIdeBar from "./SideBar";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <SIdeBar />
     </>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 
 import { FaTwitter, FaBars } from "react-icons/fa";
@@ -7,38 +7,41 @@ import Links from "./Data";
 import "./App.css";
 
 function Navbar() {
+  const [shown, SetShown] = useState(false);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={reactLogo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={() => SetShown(true)}>
             <FaBars />
           </button>
         </div>
-
-        <ul className="social-icons">
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-        </ul>
+        {shown && (
+          <ul className="social-icons">
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+          </ul>
+        )}
       </div>
       <ul>
         {Links.map((links) => {
